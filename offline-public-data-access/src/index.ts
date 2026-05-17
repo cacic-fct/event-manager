a5c69e10a5bcf89db38ce10fd7df59d5bc31e7d3
Yudi <12040397+Yudi@users.noreply.github.com>
2026-05-17 02:09:20 -0300

    Update index.ts

--- offline-public-data-access/src/index.ts
+++ offline-public-data-access/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/public-offline-database';
 export * from './lib/offline-public-data-schema';
 export * from './lib/calendar-offline-data.service';
 export * from './lib/user-offline-data.service';
+export * from './lib/offline-public-database-provider';
