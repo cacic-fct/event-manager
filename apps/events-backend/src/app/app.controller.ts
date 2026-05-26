c8bbf23c4d2d644cae32977a2377763843ac101c
Yudi <12040397+Yudi@users.noreply.github.com>
2026-05-25 23:17:27 -0300

    Redirect /api to docs root

--- apps/events-backend/src/app/app.controller.ts
+++ apps/events-backend/src/app/app.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Redirect } from '@nestjs/common';
 
 @Controller()
-export class AppController {}
+export class AppController {
+  @Get()
+  // TODO: Redirect to API docs
+  @Redirect('https://docs.fctapp.cacic.dev.br', 303)
+  getApiRoot(): void {
+    return;
+  }
+}
