96d02d8ed1a94fd0779ad2dd1f209221d0867e95
Yudi <12040397+Yudi@users.noreply.github.com>
2026-06-25 19:56:19 -0300

    Coverage

--- apps/admin/src/app/feature-flags/cookie-banner-feature-flag.service.spec.ts
+++ apps/admin/src/app/feature-flags/cookie-banner-feature-flag.service.spec.ts
@@ -1,6 +1,5 @@
 import { PLATFORM_ID } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
-import { CookieBannerFeatureFlagService } from './cookie-banner-feature-flag.service';
 
 const unleashClientMock = vi.hoisted(() => ({
   start: vi.fn<() => Promise<void>>(() => Promise.resolve()),
@@ -21,9 +20,13 @@ vi.mock('unleash-proxy-client', () => ({
 }));
 
 describe('CookieBannerFeatureFlagService', () => {
-  beforeEach(() => {
+  let CookieBannerFeatureFlagService: typeof import('./cookie-banner-feature-flag.service').CookieBannerFeatureFlagService;
+
+  beforeEach(async () => {
     TestBed.resetTestingModule();
+    vi.resetModules();
     vi.clearAllMocks();
+    CookieBannerFeatureFlagService = (await import('./cookie-banner-feature-flag.service')).CookieBannerFeatureFlagService;
 
     TestBed.configureTestingModule({
       providers: [{ provide: PLATFORM_ID, useValue: 'browser' }],
