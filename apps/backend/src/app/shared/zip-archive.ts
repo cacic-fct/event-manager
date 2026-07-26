26aae68bc87e861eda99a3bf637a042773cf6812
Yudi <12040397+Yudi@users.noreply.github.com>
2026-07-25 22:30:29 -0300

    Update zip-archive.ts

--- apps/backend/src/app/shared/zip-archive.ts
+++ apps/backend/src/app/shared/zip-archive.ts
@@ -1,4 +1,4 @@
-import type { ZipArchive } from 'archiver';
+import type { ZipArchive } from 'archiver' with { 'resolution-mode': 'import' };
 
 export type ZipArchiveStream = InstanceType<typeof ZipArchive>;
 
